135cbc112532b1a4d0d44c16f06f42f862c8063b
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-04-25 20:24:09 -0400

    Created basic page

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import Navbar from "./components/navbar"
 
 function App() {
   return (
-    <div>
+    <div className="w-full">
       <Navbar/>
       
-      <div className="min-h-screen bg-black flex items-center justify-center relative overflow-hidden">
-        
-        <div>
-          <p className="text-xl text-white font-bold">Hi, I'm</p>
-          <h1 className="text-7xl font-bold bg-gradient-to-r from-emerald-600 via-green-600 to-green-700 text-transparent bg-clip-text leading-relaxed">Cristian</h1>
-        </div>
-        
-      </div>
+      {/* First section */}
+      <div className="w-full bg-black">
 
+        <div className="max-w-screen-xl mx-auto p-4 min-h-screen flex items-center justify-center relative overflow-hidden">
+
+          <div className="absolute start-0 top-90">
+
+            <p className="text-4xl text-white font-bold">
+              Hi, I'm
+            </p>
+            <h1 className="text-7xl font-bold bg-gradient-to-r from-emerald-600 via-green-600 to-green-700 text-transparent bg-clip-text leading-relaxed">
+              Cristian Diaconu
+            </h1>
+            
+          </div>
+        </div>
+      </div>
     </div>
     
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,25 +4,25 @@ const Navbar = () => {
     return (
         <nav className="bg-gradient-to-r from-emerald-600 via-green-600 to-green-700 sticky top-0 z-50">
 
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="flex flex-wrap items-center justify-between mx-auto p-4">
                 
                 <a className="flex items-center space-x-3 rtl:space-x-reverse">
                     <span className="self-center text-2xl font-bold whitespace-nowrap">Cristian Diaconu</span>
                 </a>
 
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                <div className="w-full w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
 
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm md:border-0 md:p-0 ">About</a>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm border-0 md:p-0 ">About</a>
                         </li>
 
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm md:border-0 md:p-0">Experience</a>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm border-0 md:p-0">Experience</a>
                         </li>
 
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm md:border-0 md:p-0">Projects</a>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm border-0 md:p-0">Projects</a>
                         </li>
 
                     </ul>
