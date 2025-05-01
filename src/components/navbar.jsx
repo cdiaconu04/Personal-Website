437642bfea40485a23098d9a0a5f80102139b3e3
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-05-01 14:03:37 -0400

    Fixed navbar style

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,6 @@ import { Disc3 } from 'lucide-react';
 import { motion } from "motion/react"
 
 const Navbar = () => {
-    const [hovered, setHovered] = React.useState(false);
 
     return (
         <nav className="bg-gradient-to-r from-emerald-600 via-green-600 to-green-700 sticky top-0 z-50">
@@ -17,22 +16,26 @@ const Navbar = () => {
                 <div id="navbar-default">
 
                     <ul className="font-medium flex flex-row space-x-4 rtl:space-x-reverse">
-                        <motion.li className="flex flex-col" onHoverStart={() => setHovered(true)} onHoverEnd={() => setHovered(false)}>
-                            <a href="#" className="block py-2 px-3 text-white rounded-sm border-0 hover:text-white">About</a>
-                            <div className={`border-t-2 border-white rounded-xl transition-all duration-300 ease-in-out ${hovered ? 'w-full' : 'w-0'}`}> </div>
-                        </motion.li>
-                        <motion.li className="flex flex-col" onHoverStart={() => setHovered(true)} onHoverEnd={() => setHovered(false)}>
-                            <a href="#" className="block py-2 px-3 text-white rounded-sm border-0 hover:text-white">Experience</a>
-                            <div className={`border-t-2 border-white rounded-xl transition-all duration-300 ease-in-out ${hovered ? 'w-full' : 'w-0'}`}> </div>
-                        </motion.li>
-                        <motion.li className="flex flex-col" onHoverStart={() => setHovered(true)} onHoverEnd={() => setHovered(false)}>
-                            <a href="#" className="block py-2 px-3 text-white rounded-sm border-0 hover:text-white">Projects</a>
-                            <div className={`border-t-2 border-white rounded-xl transition-all duration-300 ease-in-out ${hovered ? 'w-full' : 'w-0'}`}> </div>
-                        </motion.li>
-                        <motion.li className="flex flex-col" onHoverStart={() => setHovered(true)} onHoverEnd={() => setHovered(false)}>
-                            <a href="#" className="block py-2 px-3 text-white rounded-sm border-0 hover:text-white">Connect</a>
-                            <div className={`border-t-2 border-white rounded-xl transition-all duration-300 ease-in-out ${hovered ? 'w-full' : 'w-0'}`}> </div>
-                        </motion.li>
+                        <li className="flex flex-col">
+                            <a href="#" className="group block py-2 px-3 text-white rounded-sm border-0 hover:text-white transition duration-300">About
+                                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-white"></span>
+                            </a>
+                        </li>
+                        <li className="flex flex-col">
+                            <a href="#" className="group block py-2 px-3 text-white rounded-sm border-0 hover:text-white transition duration-300">Experience
+                                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-white"></span>
+                            </a>
+                        </li>
+                        <li className="flex flex-col">
+                            <a href="#" className="group block py-2 px-3 text-white rounded-sm border-0 hover:text-white transition duration-300">Projects
+                                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-white"></span>
+                            </a>
+                        </li>
+                        <li className="flex flex-col">
+                            <a href="#" className="group block py-2 px-3 text-white rounded-sm border-0 hover:text-white transition duration-300">Connect
+                                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-white"></span>
+                            </a>
+                        </li>
                     </ul>
                     
                 </div>
