a71351b3f4bf743ae6e5984ec14c55a0a963c21e
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-05-04 14:25:36 -0400

    Updated intro page background

--- src/components/Intropage.jsx
+++ src/components/Intropage.jsx
@@ -111,6 +111,80 @@ const IntroPage = () => {
                         fill: "transparent",
                     }}
                 />
+
+                {/* Top right */}
+                <line x1="2000" y1="100" x2="550" y2="100" stroke="#059669" // Horizontal
+                    style={{
+                        strokeWidth: 2,
+                        strokeLinecap: "round",
+                        fill: "transparent",
+                    }}
+                />
+                <line x1="740" y1="100" x2="675" y2="160" stroke="#059669" //Diagonal
+                    style={{
+                        strokeWidth: 2,
+                        strokeLinecap: "round",
+                        fill: "transparent",
+                    }}
+                />
+                <line x1="675" y1="160" x2="470" y2="160" stroke="#059669" // Horizontal
+                    style={{
+                        strokeWidth: 2,
+                        strokeLinecap: "round",
+                        fill: "transparent",
+                    }}
+                />
+                <circle cx="545" cy="100" r="5" stroke="#FFFFFF"
+                    style={{
+                        strokeWidth: 2,
+                        strokeLinecap: "round",
+                        fill: "transparent",
+                    }}
+                />
+                <circle cx="465" cy="160" r="5" stroke="#FFFFFF"
+                    style={{
+                        strokeWidth: 2,
+                        strokeLinecap: "round",
+                        fill: "transparent",
+                    }}
+                />
+
+                {/* Top left */}
+                <line x1="2000" y1="550" x2="400" y2="550" stroke="#059669" // Horizontal
+                    style={{
+                        strokeWidth: 2,
+                        strokeLinecap: "round",
+                        fill: "transparent",
+                    }}
+                />
+                <circle cx="395" cy="550" r="5" stroke="#FFFFFF"
+                    style={{
+                        strokeWidth: 2,
+                        strokeLinecap: "round",
+                        fill: "transparent",
+                    }}
+                />
+                <line x1="500" y1="490" x2="565" y2="550" stroke="#059669" //Diagonal
+                    style={{
+                        strokeWidth: 2,
+                        strokeLinecap: "round",
+                        fill: "transparent",
+                    }}
+                />
+                <line x1="500" y1="490" x2="350" y2="490" stroke="#059669" // Horizontal
+                    style={{
+                        strokeWidth: 2,
+                        strokeLinecap: "round",
+                        fill: "transparent",
+                    }}
+                />
+                <circle cx="345" cy="490" r="5" stroke="#FFFFFF"
+                    style={{
+                        strokeWidth: 2,
+                        strokeLinecap: "round",
+                        fill: "transparent",
+                    }}
+                />
             
                 {/* <circle cx="-330" cy="200" r="30" stroke="#FFFFFF"
                     style={{
