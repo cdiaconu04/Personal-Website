5e1d24334c96981f30c8a21269d8ad00f6b18195
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-05-05 15:00:26 -0400

    Added animate-in effect for intropage text and pic

--- src/components/Intropage.jsx
+++ src/components/Intropage.jsx
@@ -1,10 +1,10 @@
 import headshot from '../images/1727488282356.jpg';
 import '../App.css';
-import { motion } from "motion/react"
+import { motion, useInView, useAnimate, useAnimation } from "motion/react"
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import IntroBackground from './backgrounds/IntroBackground'
 
@@ -13,61 +13,92 @@ import SplitType from 'split-type'
 import { gsap } from "gsap";
 
 const IntroPage = () => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+    const mainControls = useAnimation();
+
+    useEffect(() => {
+        if (isInView) {
+            mainControls.start("visible");
+        }
+    }, [isInView]);
 
     return (
         <div className="w-full bg-black">
             <IntroBackground/>
 
-            <div className="max-w-screen-xl mx-auto p-4 min-h-screen flex items-center justify-between relative overflow-hidden">
+            <div 
+                className="max-w-screen-xl mx-auto p-4 min-h-screen flex items-center justify-between relative overflow-hidden"
+                ref={ref}
+            >
             
-            <motion.div>
-
-                <motion.p className="text-4xl text-white font-bold" 
+                <motion.div
+                    variants={{
+                        hidden: { opacity: 0, y: 75 },
+                        visible: { opacity: 1, y: 0 },
+                    }}
+                    initial="hidden"
+                    animate={mainControls}
+                    transition={{ duration: 0.8, delay: 0.25 }}
                 >
-                Hello, I'm
-                </motion.p>
 
-                <motion.h1 className="text-7xl font-bold bg-gradient-to-r from-emerald-600 via-green-600 to-green-700 text-transparent bg-clip-text leading-relaxed "
-                >
-                Cristian Diaconu
-                </motion.h1>
-
-                <br/>
-
-                <div className="justify-start flex space-x-3">
-
-                    <a href="https://linkedin.com/in/cristian-diaconu04" target="_blank">
-                        <motion.div
-                        whileHover={{ scale: 1.09 }}
-                        >
-                        <FaLinkedin color="white" className="w-8 h-8"/>
-                        </motion.div>
-                    </a>
                     
-                    <a href="https://github.com/CristianDiaconu04" target="_blank">
-                        <motion.div
-                        whileHover={{ scale: 1.09 }}>
-                        <FaGithub color="white" className="w-8 h-8"/>
-                        </motion.div>
-                    </a>
 
-                    {/* <div className="outline-1 rounded-lg p-1 px-2">
-                        <p className="font-bold bg-gradient-to-r from-emerald-600 via-green-600 to-green-700 text-transparent bg-clip-text leading-relaxed ">
-                            Software Developer
-                        </p>
-                    </div> */}
+                    <motion.p className="text-4xl text-white font-bold" 
+                    >
+                    Hello, I'm
+                    </motion.p>
 
-                </div>
+                    <motion.h1 className="text-7xl font-bold bg-gradient-to-r from-emerald-600 via-green-600 to-green-700 text-transparent bg-clip-text leading-relaxed "
+                    >
+                    Cristian Diaconu
+                    </motion.h1>
+
+                    <br/>
+
+                    <div className="justify-start flex space-x-3">
+
+                        <a href="https://linkedin.com/in/cristian-diaconu04" target="_blank">
+                            <motion.div
+                            whileHover={{ scale: 1.09 }}
+                            >
+                            <FaLinkedin color="white" className="w-8 h-8"/>
+                            </motion.div>
+                        </a>
+                        
+                        <a href="https://github.com/CristianDiaconu04" target="_blank">
+                            <motion.div
+                            whileHover={{ scale: 1.09 }}>
+                            <FaGithub color="white" className="w-8 h-8"/>
+                            </motion.div>
+                        </a>
+
+                        <div className="border border-green-700 rounded-full p-1 px-2">
+                            <p className="font-bold bg-gradient-to-r from-emerald-600 via-green-600 to-green-700 text-transparent bg-clip-text leading-relaxed ">
+                                Software Developer
+                            </p>
+                        </div>
+
+                    </div>
+                    
+                    
+                </motion.div>
                 
-                
-            </motion.div>
-            
-            <motion.div className="relative">
-                <div className="absolute inset-0 bg-green-400 rounded-full blur"></div>
-                <div>
-                <img className="relative rounded-full ms-auto w-80 h-80" src={headshot} />
-                </div>
-            </motion.div>
+                <motion.div 
+                    className="relative"
+                    variants={{
+                        hidden: { opacity: 0, y: 75 },
+                        visible: { opacity: 1, y: 0 },
+                    }}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ duration: 0.8, delay: 0.25 }}
+                >
+                    <div className="absolute inset-0 bg-green-400 rounded-full blur"></div>
+                    <div>
+                    <img className="relative rounded-full ms-auto w-80 h-80" src={headshot} />
+                    </div>
+                </motion.div>
 
             </div>
 
