7ee990f2f0142354dea05c66760e221cc2c32483
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-07-01 19:10:58 -0400

    Updated mobile colours for exp and proj

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -74,7 +74,10 @@ const Projects = forwardRef(({windowSize}, ref) => {
                             
                             {/* Proj1 */}
                             <motion.div whileHover={{ y: -7 }}>
-                                <motion.div className="bg-gradient-to-r from-emerald-400 via-emerald-400 to-green-400 dark:bg-gradient-to-r dark:from-emerald-600 dark:via-emerald-600 dark:to-green-600 rounded-lg min-w-[32%] transition duration-200">
+                                <motion.div className="rounded-lg min-w-[32%] transition duration-200
+                                    sm:bg-gradient-to-r sm:from-emerald-400 sm:via-emerald-400 sm:to-green-400 sm:dark:bg-gradient-to-r sm:dark:from-emerald-600 sm:dark:via-emerald-600 sm:dark:to-green-600 
+                                    dark:bg-gradient-to-r dark:from-emerald-600 dark:via-green-600 dark:to-green-700 bg-gradient-to-r from-emerald-400 via-green-400 to-green-500
+                                ">
 
                                     <div className="inline-block overflow-hidden">
                                         <img src={CodeBasketPic} className="w-full rounded-t-lg transition-transform duration-500"/>
@@ -142,7 +145,10 @@ const Projects = forwardRef(({windowSize}, ref) => {
 
                             {/* Eldi store */}
                             <motion.div whileHover={{ y: -7 }}>
-                                <motion.div className="bg-green-400 dark:bg-green-600 rounded-lg min-w-[32%] transition duration-200">
+                                <motion.div className="rounded-lg min-w-[32%] transition duration-200
+                                    sm:bg-green-400 sm:dark:bg-green-600 
+                                    dark:bg-gradient-to-r dark:from-emerald-600 dark:via-green-600 dark:to-green-700 bg-gradient-to-r from-emerald-400 via-green-400 to-green-500
+                                ">
                                     <div className="inline-block overflow-hidden">
                                         {/* hover:scale-105 */}
                                         <img src={EldiEcomStore} className="w-full rounded-t-lg transition-transform duration-500"/>
@@ -198,7 +204,10 @@ const Projects = forwardRef(({windowSize}, ref) => {
                             {/* className="bg-gradient-to-r from-green-400 via-green-500 to-green-500 dark:bg-gradient-to-r dark:from-green-600 dark:via-green-700 dark:to-green-700 rounded-lg p-7 min-w-[32%] transition duration-200" */}
                             {/* Project 3 */}
                             <motion.div whileHover={{ y: -7 }}>
-                                <motion.div className="bg-gradient-to-r from-green-400 via-green-500 to-green-500 dark:bg-gradient-to-r dark:from-green-600 dark:via-green-700 dark:to-green-700 rounded-lg min-w-[32%] transition duration-200">
+                                <motion.div className="rounded-lg min-w-[32%] transition duration-200
+                                    sm:bg-gradient-to-r sm:from-green-400 sm:via-green-500 sm:to-green-500 sm:dark:bg-gradient-to-r sm:dark:from-green-600 sm:dark:via-green-700 sm:dark:to-green-700 
+                                    dark:bg-gradient-to-r dark:from-emerald-600 dark:via-green-600 dark:to-green-700 bg-gradient-to-r from-emerald-400 via-green-400 to-green-500
+                                ">
                                     <div className="inline-block overflow-hidden">
                                         <img src={YoutubeDownloader} className="w-full rounded-t-lg transition-transform duration-500"/>
                                     </div>
